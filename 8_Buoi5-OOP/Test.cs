487b85860978923a447b3c6ceccb0d3c18f6ccd7
Tphatt007 <phantanphat1308@gmmail.com>
2026-09-05 15:11:24 +0700

    complete buoi5

---
+++ 8_Buoi5-OOP/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace Buoi5_OOP
+{
+    class Test
+    {
+        /* public static void Main(string[] args)
+         {
+             Console.OutputEncoding = Encoding.UTF8;
+             Die d = new Die();
+             Console.WriteLine(d);
+         }*/
+    }
+    
+}
